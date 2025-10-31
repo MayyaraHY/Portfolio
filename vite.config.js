b1fdcd788c9d80bbdb0ba3445017f5833e67e3e6
MayyaraHY <mayyara.hajyahia@isimg.tn>
2025-10-31 18:01:02 +0100

    picture routing fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: "/Portfolio", // serve from GitHub Pages at /Portfolio/
+    publicDir: 'public',
 })
